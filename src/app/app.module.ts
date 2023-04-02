77bfb1b8dc18634814d5306d233045840dc3bd8a
Pedro Henrique Araujo <pedro.henrique@easyteam.rocks>
2023-04-02 10:37:29 -0300

    header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageHeaderComponent } from './components/page-header/page-header.component';
+import { PageFooterComponent } from './components/page-footer/page-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageHeaderComponent,
+    PageFooterComponent
   ],
   imports: [
     BrowserModule,
